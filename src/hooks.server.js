0c8656b85088ae2e6bc2f2a22f0074d2a1c668b4
PivotStick <pivoorigami@gmail.com>
2024-05-26 18:23:13 +0200

    webcontainer test

---
+++ src/hooks.server.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("@sveltejs/kit").Handle}
+ */
+export async function handle({ event, resolve }) {
+	const response = await resolve(event);
+
+	response.headers.set('cross-origin-opener-policy', 'same-origin');
+	response.headers.set('cross-origin-embedder-policy', 'require-corp');
+	response.headers.set('cross-origin-resource-policy', 'cross-origin');
+
+	return response;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { WebContainer } from '@webcontainer/api';
+	import { onMount } from 'svelte';
+
+	/**
+	 * @type {HTMLIFrameElement}
+	 */
+	let iframe;
+
+	onMount(async () => {
+		const vm = await WebContainer.boot();
+
+		await vm.mount({
+			'index.js': {
+				file: {
+					contents: `console.log("Yoooo")`
+				}
+			}
+		});
+
+		const process = await vm.spawn('node', ['index.js']);
+
+		process.output.pipeTo(
+			new WritableStream({
+				write(data) {
+					console.log(data);
+				}
+			})
+		);
+	});
+</script>
+
+<iframe title="Output" src="" bind:this={iframe}></iframe>
