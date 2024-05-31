431a2da88de6373884f8c9318fcddcde3d3d10cc
mdinatale <mdinatale@encaps.io>
2024-05-31 10:51:30 +0200

    menu navigation done!

--- src/routes/tutorial/[slug]/+page.server.js
+++ src/routes/tutorial/[slug]/+page.server.js
@@ -6,9 +6,15 @@ import deepmerge from 'deepmerge';
  * @type {import("./$types").PageServerLoad}
  */
 export async function load({ params }) {
-	for (const part of content.parts) {
-		for (const chapter of part.chapters) {
-			for (const exercise of chapter.exercises) {
+	for (let partIndex = 0; partIndex < content.parts.length; partIndex++) {
+		const part = content.parts[partIndex];
+
+		for (let chapterIndex = 0; chapterIndex < part.chapters.length; chapterIndex++) {
+			const chapter = part.chapters[chapterIndex];
+
+			for (let exerciseIndex = 0; exerciseIndex < chapter.exercises.length; exerciseIndex++) {
+				const exercise = chapter.exercises[exerciseIndex];
+
 				if (exercise.slug === params.slug) {
 					return {
 						index: content,
@@ -29,6 +35,21 @@ export async function load({ params }) {
 								markdown: exercise.markdown,
 								common: deepmerge(part.common, chapter.common),
 								focus: chapter.focus,
+
+								previous:
+									(
+										chapter.exercises[exerciseIndex - 1] ??
+										part.chapters[chapterIndex - 1]?.exercises[-1] ??
+										content.parts[partIndex - 1]?.chapters[-1]?.exercises[-1]
+									)?.slug ?? null,
+
+								next:
+									(
+										chapter.exercises[exerciseIndex + 1] ??
+										part.chapters[chapterIndex + 1]?.exercises[0] ??
+										content.parts[partIndex + 1]?.chapters[0]?.exercises[0]
+									)?.slug ?? null,
+
 								'app-a': exercise['app-a'],
 								'app-b': exercise['app-b'] ?? null
 							}
--- src/routes/tutorial/[slug]/Menu.svelte
+++ src/routes/tutorial/[slug]/Menu.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { page } from '$app/stores';
 	import { expoOut } from 'svelte/easing';
 	import { slide } from 'svelte/transition';
 
@@ -11,13 +12,45 @@
 
 	const partIndex = $derived(data.index.parts.findIndex((p) => p.slug === data.content.part.slug));
 
-	$inspect(data);
-
 	let open = $state(false);
+
+	/**
+	 * @param {HTMLElement} node
+	 * @param {(event: MouseEvent) => void} callback
+	 */
+	function clickoutside(node, callback) {
+		/**
+		 * @param {MouseEvent} e
+		 */
+		function click(e) {
+			if (e.target instanceof Node && !node.contains(e.target)) {
+				callback(e);
+			}
+		}
+
+		window.addEventListener('click', click);
+
+		return {
+			destroy() {
+				window.removeEventListener('click', click);
+			}
+		};
+	}
+
+	/**
+	 * @type {HTMLElement=}
+	 */
+	let navElement = $state(undefined);
 </script>
 
 <div class="container">
-	<button class="fa fa-arrow-left"></button>
+	<a
+		href={data.content.exercise.previous
+			? `/tutorial/${data.content.exercise.previous}`
+			: undefined}
+	>
+		<i class="fa fa-arrow-left"></i>
+	</a>
 	<div
 		class="menu"
 		class:open
@@ -25,9 +58,9 @@
 		tabindex="0"
 		onkeydown={(e) => e.key === 'Enter' && (open = !open)}
 		onclick={(e) => {
-			if (e.target === e.currentTarget) open = !open;
+			if (e.currentTarget.contains(e.target) && !navElement?.contains(e.target)) open = !open;
 		}}
-		onblur={() => (open = false)}
+		use:clickoutside={() => (open = false)}
 	>
 		<p>
 			<span>Part {partIndex + 1}</span>
@@ -40,26 +73,58 @@
 		<i class="fa fa-chevron-down"></i>
 
 		{#if open}
-			<div transition:slide={{ easing: expoOut }} class="sub-menu">hahahaa</div>
+			<nav transition:slide={{ easing: expoOut }} bind:this={navElement}>
+				<ul class="parts">
+					{#each data.index.parts as part, partIndex}
+						<li class="part">
+							<button>Part {partIndex + 1}: {part.title}</button>
+							<ul class="chapters">
+								{#each part.chapters as chapter}
+									<li class="chapter">
+										<button><i class="fa fa-chevron-right"></i> {chapter.title}</button>
+										<ul class="exercises">
+											{#each chapter.exercises as exercise}
+												<li
+													class="exercise"
+													aria-current={$page.params.slug === exercise.slug ? 'page' : undefined}
+												>
+													<a href="/tutorial/{exercise.slug}" onclick={() => (open = false)}>
+														{exercise.title}
+													</a>
+												</li>
+											{/each}
+										</ul>
+									</li>
+								{/each}
+							</ul>
+						</li>
+					{/each}
+				</ul>
+			</nav>
 		{/if}
 	</div>
-	<button class="fa fa-arrow-right"></button>
+	<a href={data.content.exercise.next ? `/tutorial/${data.content.exercise.next}` : undefined}>
+		<i class="fa fa-arrow-right"></i>
+	</a>
 </div>
 
 <style lang="scss">
 	.container {
 		display: flex;
 		justify-content: space-between;
-		gap: 2rem;
-		padding: 2rem;
+		align-items: center;
+		gap: 1rem;
+		padding: 1rem 2rem;
 
 		padding-bottom: 0;
 
-		> button {
-			height: auto;
-			width: auto;
-			padding: 0;
-			background: none;
+		> a {
+			cursor: pointer;
+
+			&:not([href]) {
+				pointer-events: none;
+				opacity: 0.25;
+			}
 		}
 
 		.menu {
@@ -84,7 +149,7 @@
 				border-bottom-left-radius: 0;
 				border-bottom-right-radius: 0;
 
-				i {
+				> i {
 					rotate: 180deg;
 				}
 			}
@@ -112,15 +177,62 @@
 				}
 			}
 
-			.sub-menu {
+			nav {
+				padding: 1rem 0;
+
+				cursor: initial;
 				position: absolute;
-				padding: 1rem;
 				border: inherit;
 				border-radius: 0 0 0.25rem 0.25rem;
 				background-color: rgb(var(--color-900));
 				left: -1px;
 				right: -1px;
 				top: 100%;
+
+				overflow: auto;
+				max-height: 25rem;
+
+				button {
+					background: none;
+					padding: 0;
+					font-size: 1rem;
+				}
+
+				ul {
+					padding-left: 1.5rem;
+				}
+
+				li {
+					margin-top: 0.25rem;
+				}
+
+				.parts {
+					color: rgb(var(--color-400));
+
+					.part {
+						> button {
+							font-weight: 700;
+						}
+
+						.chapter {
+							> button {
+								font-weight: 700;
+								i {
+									font-size: 0.8rem;
+								}
+							}
+
+							.exercise {
+								padding-left: 1rem;
+
+								&[aria-current='page'] {
+									pointer-events: none;
+									color: rgb(var(--color-50));
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
