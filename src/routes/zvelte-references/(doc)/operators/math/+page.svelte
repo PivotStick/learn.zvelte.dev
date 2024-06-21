b63d8b66f3ed8c471ec8cbe097e5dec54ecd8238
mdinatale <mdinatale@encaps.io>
2024-06-21 10:22:32 +0200

    added math docs

---
+++ src/routes/zvelte-references/(doc)/operators/math/+page.svelte
@@ -0,0 +1,45 @@
+<p>Twig allows you to do math in templates; the following operators are supported:</p>
+
+<ul>
+	<li>
+		<code>+</code> Adds two numbers together (the operands are casted to numbers). {'{{ 1 + 1 }}'} is
+		2.
+	</li>
+	<li>
+		<code>-</code> Subtracts the second number from the first one. {'{{ 3 - 2 }}'} is 1.
+	</li>
+
+	<li>
+		<code>/</code> Divides two numbers. The returned value will be a floating point number. {'{{ 1 / 2 }}'}
+		is {'{{ 0.5 }}'}.
+	</li>
+
+	<li class="wip">
+		<code>%</code> Calculates the remainder of an integer division. {'{{ 11 % 7 }}'} is 4.
+	</li>
+
+	<li class="wip">
+		<code>//</code> Divides two numbers and returns the floored integer result. {'{{ 20 // 7 }}'} is
+		2, {'{{ -20 // 7 }}'}
+		is -3 (this is just syntactic sugar for the round filter).
+	</li>
+
+	<li>
+		<code>*</code> Multiplies the left operand with the right one. {'{{ 2 * 2 }}'} would return 4.
+	</li>
+
+	<li class="wip">
+		<code>**</code> Raises the left operand to the power of the right operand. {'{{ 2 ** 3 }}'} would
+		return 8.
+	</li>
+</ul>
+
+<style lang="scss">
+	.wip {
+		opacity: 0.3;
+
+		&::after {
+			content: ' --- (not implemented yet)';
+		}
+	}
+</style>
--- src/routes/zvelte-references/+page.svelte
+++ src/routes/zvelte-references/+page.svelte
@@ -157,8 +157,7 @@
 				{
 					name: 'Math',
 					slug: 'math',
-					text: '(+, -, /, *)',
-					wip: true
+					text: '(+, -, /, *)'
 				},
 				{
 					name: 'Logic',
